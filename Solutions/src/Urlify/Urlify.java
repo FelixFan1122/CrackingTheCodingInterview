b6ba1987fc320c82922bf7b4aec9804345b64a9a
FelixFan <zeus_loveship@126.com>
2017-11-21 15:36:13 +0800

    Correct a misunderstanding of the problem

--- Solutions/src/Urlify/Urlify.java
+++ Solutions/src/Urlify/Urlify.java
@@ -3,11 +3,6 @@ package com.felix.ctci.Urlify;
 public class Urlify {
     private static final String Space = "%20";
 
-    private static boolean isWhitespace(char character) {
-        // TODO: Expand this.
-        return character == ' ' || character == '\t';
-    }
-
     public static void main(String[] args) {
         System.out.println(String.format("expected = %s, actual = %s", "Mr%20John%20Smith", urlify("Mr John Smith   ")));
     }
@@ -18,11 +13,7 @@ public class Urlify {
         boolean isUrlified = false;
         for (int i = 0; i < trimmed.length(); i++) {
             char character = trimmed.charAt(i);
-            if (isWhitespace(character)) {
-                if (isUrlified) {
-                    continue;
-                }
-
+            if (character == ' ' && !isUrlified) {
                 urlified.append(Space);
                 isUrlified = true;
             } else {
