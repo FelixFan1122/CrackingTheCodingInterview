c4455d37649819600d4a3695b8ab5e7eedb99413
FelixFan <zeus_loveship@126.com>
2017-12-08 15:02:22 +0800

    Modify the solution to Intersection after peeking at hints

--- Solutions/src/Intersection/Intersection.java
+++ Solutions/src/Intersection/Intersection.java
@@ -2,38 +2,50 @@ package com.felix.ctci.Intersection;
 
 import com.felix.ctci.CodeLibrary.Node;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Intersection {
     public static boolean hasIntersection(Node linkedList1, Node linkedList2) {
-        if (linkedList1 == null || linkedList2 == null) {
-            return false;
+        int length1 = getLength(linkedList1);
+        int length2 = getLength(linkedList2);
+
+        Node longList;
+        Node shortList;
+        if (length1 < length2) {
+            longList = linkedList2;
+            shortList = linkedList1;
+        } else {
+            longList = linkedList1;
+            shortList = linkedList2;
         }
 
-        HashMap<Node, ArrayList<Node>> nodes = new HashMap<>();
-        while (linkedList1.getNext() != null) {
-            if (nodes.containsKey(linkedList1)) {
-                nodes.get(linkedList1).add(linkedList1);
-            } else {
-                ArrayList<Node> list = new ArrayList<>();
-                list.add(linkedList1);
-                nodes.put(linkedList1, list);
-            }
-
-            linkedList1 = linkedList1.getNext();
+        int difference = length1 < length2 ? length2 - length1 : length1 - length2;
+        while (difference > 0) {
+            difference--;
+            longList = longList.getNext();
         }
 
-        while (linkedList2.getNext() != null) {
-            if (nodes.containsKey(linkedList2)) {
-                if (nodes.get(linkedList2).contains(linkedList2)) {
-                    return true;
-                }
+        while (longList.getNext() != null) {
+            if (longList.getData() == shortList.getData()) {
+                return true;
             }
 
-            linkedList2 = linkedList2.getNext();
+            longList = longList.getNext();
+            shortList = shortList.getNext();
         }
 
         return false;
+    }
+
+    private static int getLength(Node linkedList) {
+        if (linkedList == null) {
+            return 0;
+        }
+
+        int length = 1;
+        while (linkedList.getNext() != null) {
+            linkedList = linkedList.getNext();
+            length++;
+        }
+
+        return length;
     }
 }
