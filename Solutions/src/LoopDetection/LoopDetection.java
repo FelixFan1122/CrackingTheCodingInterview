784b301090993847eca8dcfd36ddf4b73376f417
FelixFan <zeus_loveship@126.com>
2017-12-09 10:10:56 +0800

    Update solution to Loop Detection with the algorithm in the book

--- Solutions/src/LoopDetection/LoopDetection.java
+++ Solutions/src/LoopDetection/LoopDetection.java
@@ -2,20 +2,29 @@ package com.felix.ctci.LoopDetection;
 
 import com.felix.ctci.CodeLibrary.Node;
 
-import java.util.HashSet;
-
 public class LoopDetection {
     public static Node detectLoop(Node linkedList) {
-        HashSet<Node> previousNodes = new HashSet<>();
-        while (linkedList != null) {
-            if (previousNodes.contains(linkedList)) {
-                return linkedList;
-            }
-
-            previousNodes.add(linkedList);
-            linkedList = linkedList.getNext();
+        if (linkedList == null || linkedList.getNext() == null || linkedList.getNext().getNext() == null) {
+            return null;
         }
 
-        return null;
+        Node current = linkedList.getNext();
+        Node runner = linkedList.getNext().getNext();
+        while (current != runner && runner.getNext() != null && runner.getNext().getNext() != null) {
+            current = current.getNext();
+            runner = runner.getNext().getNext();
+        }
+
+        if (current != runner) {
+            return null;
+        }
+
+        current = linkedList;
+        while (current != runner) {
+            current = current.getNext();
+            runner = runner.getNext();
+        }
+
+        return current;
     }
 }
