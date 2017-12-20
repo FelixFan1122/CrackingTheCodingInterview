2d0fe3a98b5d2eae43a7898e812767aebdfa3811
FelixFan <zeus_loveship@126.com>
2017-12-20 12:06:09 +0800

    Update the solution to Successor again after reading the solution in the book.

--- Solutions/src/Successor/Successor.java
+++ Solutions/src/Successor/Successor.java
@@ -6,7 +6,20 @@ import javax.swing.*;
 
 public class Successor {
     public static <E> TreeNode<E> getSuccessorInOrder(TreeNode<E> bst) {
-        if (bst.getRightChild() != null) {
+        if (bst == null) {
+            return null;
+        }
+
+        if (bst.getRightChild() == null) {
+            TreeNode<E> current = bst;
+            TreeNode<E> parent = bst.getParent();
+            while (parent != null && parent.getLeftChild() != current) {
+                current = parent;
+                parent = current.getParent();
+            }
+
+            return parent;
+        } else {
             TreeNode<E> current = bst.getRightChild();
             while (current.getLeftChild() != null) {
                 current = current.getLeftChild();
@@ -14,14 +27,5 @@ public class Successor {
 
             return current;
         }
-
-        TreeNode<E> current = bst;
-        TreeNode<E> parent = bst.getParent();
-        while (parent != null && parent.getLeftChild() != current) {
-            current = parent;
-            parent = current.getParent();
-        }
-
-        return parent == null ? current : parent;
     }
 }
