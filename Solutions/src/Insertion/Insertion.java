e41cb10a6e75fb56b373900046778f3b39a19d06
FelixFan <zeus_loveship@126.com>
2018-01-09 11:46:20 +0800

    Correct solution to Insertion after reading the answer in the book

--- Solutions/src/Insertion/Insertion.java
+++ Solutions/src/Insertion/Insertion.java
@@ -2,9 +2,8 @@ package com.felix.ctci.Insertion;
 
 public class Insertion {
     public static int insert(int m, int n, int i, int j) {
-        int mask = (-1 << j) & (-1 >>> (32 - i));
+        int mask = (-1 << j) | (-1 >>> (32 - i));
         int maskedN = mask & n;
-        int maskedM = mask | (m << i);
-        return maskedN & maskedM;
+        return maskedN | (m << i);
     }
 }
