9ec1bd38617bf2125508937873931ec76193d476
FelixFan <zeus_loveship@126.com>
2018-02-01 11:15:23 +0800

    Rectify solution to Magic Index

--- Solutions/src/MagicIndex/MagicIndex.java
+++ Solutions/src/MagicIndex/MagicIndex.java
@@ -2,17 +2,29 @@ package com.felix.ctci.MagicIndex;
 
 public class MagicIndex {
     public static int getMagicIndex(int[] a) {
-        return getMagicIndex(a, 0, a.length);
+        return getMagicIndex(a, 0, a.length - 1);
     }
 
     private static int getMagicIndex(int[] a, int start, int end) {
-        int mid = start + (end - start) / 2;
+        if (start < end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
         if (a[mid] == mid) {
             return mid;
-        } else if (a[mid] > mid) {
-            return getMagicIndex(a, start, mid);
-        } else {
-            return getMagicIndex(a, mid, end);
         }
+
+        int left = getMagicIndex(a, start, mid - 1);
+        if (left >= 0) {
+            return left;
+        }
+
+        int right = getMagicIndex(a, mid + 1, end);
+        if (right >= 0) {
+            return right;
+        }
+
+        return -1;
     }
 }
