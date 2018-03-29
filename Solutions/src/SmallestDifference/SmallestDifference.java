c795f67de81d7b1f6a5df22b2b6af3c213069814
FelixFan <zeus_loveship@126.com>
2018-03-29 17:17:56 +0800

    Add solution to Smallest Difference

---
+++ Solutions/src/SmallestDifference/SmallestDifference.java
@@ -0,0 +1,24 @@
+package com.felix.ctci.SmallestDifference;
+
+import java.util.Arrays;
+
+public class SmallestDifference {
+    public static int getSmallestDifference(int[] input1, int[] input2) {
+        Arrays.sort(input1);
+        Arrays.sort(input2);
+        int smallestDifference = Integer.MAX_VALUE;
+        for (int i = 0, j = 0; i < input1.length || j < input2.length;) {
+            if (Math.abs(input1[i] - input2[j]) < smallestDifference) {
+                smallestDifference = Math.abs(input1[i] - input2[j]);
+            }
+
+            if (input1[i] < input2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return smallestDifference;
+    }
+}
