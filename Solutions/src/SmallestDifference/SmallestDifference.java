0cc6bf11be9ef453efd50867de04dfe49efeeebf
FelixFan <zeus_loveship@126.com>
2018-03-29 17:21:25 +0800

    Rectify the solution to Smallest Difference

--- Solutions/src/SmallestDifference/SmallestDifference.java
+++ Solutions/src/SmallestDifference/SmallestDifference.java
@@ -7,7 +7,7 @@ public class SmallestDifference {
         Arrays.sort(input1);
         Arrays.sort(input2);
         int smallestDifference = Integer.MAX_VALUE;
-        for (int i = 0, j = 0; i < input1.length || j < input2.length;) {
+        for (int i = 0, j = 0; i < input1.length && j < input2.length;) {
             if (Math.abs(input1[i] - input2[j]) < smallestDifference) {
                 smallestDifference = Math.abs(input1[i] - input2[j]);
             }
